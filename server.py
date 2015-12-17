72a365daa76db38866594388e0975f5f00b50b27
Alfin Akhret <alfin@qasico.com>
2015-12-17 15:45:12 +0700

    accept multiple client connection using thread

--- server.py
+++ server.py
@@ -2,29 +2,42 @@
 
 import socket
 import sys
+from _thread import *
 
 HOST, PORT, RECV_BUFFER = '', 8888, 1024
 
 # create socket object
 # make it listen
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
-s.listen(1)
+print('Socket created')
+   
+try:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Socket binding failed. Error code:{err_code}. Error message:{err_msg}'.format(err_code=str(msg[0]), err_msg=str(msg[1])))
+    sys.exit()
 
+s.listen(1)
 print('server is listening on {host}:{port}...'.format(host=s.getsockname()[0], port=str(s.getsockname()[1])))
+
+# client thread
+# use to maintain connection with connected client
+# using threaded-server technique
+def client_thread(client_conn):
+    while True:
+        data = client_conn.recv(RECV_BUFFER)
+        if not data:
+            break
+        resp = b'Ok...' + data
+        client_conn.sendall(resp)
+    cleint_conn.close()
 
 # listen for new incoming connection
 while True:
     client_conn, client_addr = s.accept()
     print('A client connected from {}'.format(client_addr))
-    # receive incoming message from client
-    message = client_conn.recv(RECV_BUFFER)
-    # send response
-    resp = b'OK...' + message
-    client_conn.sendall(resp)
-    # close connected socket
-    client_conn.close()
+    start_new_thread(client_thread, (client_conn,))
 
 # close listening socket
 s.close()
