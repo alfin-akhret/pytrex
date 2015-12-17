36f258995c0976fdb37d07cbba7ec46b54483e05
Alfin Akhret <alfin@qasico.com>
2015-12-17 18:27:09 +0700

    change threaded server into asynchronous one using kernel select()

--- app.py
+++ app.py
@@ -3,6 +3,6 @@
 from pytrex import httpserver
 
 if __name__ == '__main__':
-    s = httpserver.server()
+    s = httpserver.server(5)
     s.serv_forever()
 
--- pytrex/httpserver.py
+++ pytrex/httpserver.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import socket
+import select
 import sys
 from _thread import *
 
 HOST, PORT, RECV_BUFFER = '', 8888, 1024
+CONNECTION_LIST = []
+backlog = 5
 
 
 class server(object):
-    def __init__(self):        
+    def __init__(self, backlog = 5):        
         # create a socket
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,24 +29,42 @@ class server(object):
             print('Error binding socket: %s' % e)
             sys.exit(1)
 
-    def serv_forever(self, max_conn=1):
-        self.socket.listen(max_conn)
+    def serv_forever(self):
+        self.socket.listen(backlog)
+        CONNECTION_LIST.append(self.socket)
         while True:
-            client_conn, client_addr = self.socket.accept()
-            print('A client connected from {}'.format(client_addr))
-            start_new_thread(self.client_thread, (client_conn,))
+            try:
+                readable, writeable, exceptional = select.select(CONNECTION_LIST, [],[])
+            except select.error as e:
+                break
+
+            for sock in readable:
+                # if readable socket is the listening socket
+                # it means a new client is trying to connect
+                # handle it
+                if sock == self.socket:
+                    client_conn, client_addr = self.socket.accept()
+                    # add new connected client to the list
+                    CONNECTION_LIST.append(client_conn)
+                    print('A client connected from {}'.format(client_addr))
+                else:
+                    # handle already connected clients
+                    try:
+                        buf = sock.recv(RECV_BUFFER)
+                    except socket.error as e:
+                        print('Error receiving data %s' % e)
+                        sys.exit(1)
+                    if not len(buf):
+                        break
+                    resp = b'Ok...' + buf
+                    try:
+                        sock.sendall(resp)
+                    except socket.error as e:
+                        print('Error sending data %s' % e)
+                        sock.close()
+                        CONNECTION_LIST.remove(sock)
+                        continue
+
         self.socket.close()
 
-    def client_thread(self, client_conn):
-        while True:
-            # receive data from client
-            try:
-                buf = client_conn.recv(RECV_BUFFER)
-            except socket.error as e:
-                print('Error receiving data: %s' % e)
-                sys.exit(1)
-            if not len(buf):
-                break
-            resp = b'Ok...' + buf 
-            client_conn.sendall(resp)
-        client_conn.close()
+    
