fb8a06a0f3f2f3cff540ff8ecc7b5553b36365b3
Alfin Akhret <alfin@qasico.com>
2015-12-17 17:11:45 +0700

    add some exception handler

--- pytrex/httpserver.py
+++ pytrex/httpserver.py
@@ -9,14 +9,22 @@ HOST, PORT, RECV_BUFFER = '', 8888, 1024
 
 class server(object):
     def __init__(self):        
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # create a socket
         try:
-            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as e:
+            print('Error creating socket: %s' % e)
+            sys.exit(1)
+
+        # set socket options
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # bind the socket
+        try:
             self.socket.bind((HOST, PORT))
-        except socket.error as msg:
-            print('Socket binding failed. Error code:{err_code}. Error message:{err_msg}'.format(err_code=str(msg[0]), err_msg=str(msg[1])))
-            sys.exit()
-        print('Socket created')
+        except socket.error as e:
+            print('Error binding socket: %s' % e)
+            sys.exit(1)
 
     def serv_forever(self, max_conn=1):
         self.socket.listen(max_conn)
@@ -28,9 +36,14 @@ class server(object):
 
     def client_thread(self, client_conn):
         while True:
-            data = client_conn.recv(RECV_BUFFER)
-            if not data:
+            # receive data from client
+            try:
+                buf = client_conn.recv(RECV_BUFFER)
+            except socket.error as e:
+                print('Error receiving data: %s' % e)
+                sys.exit(1)
+            if not len(buf):
                 break
-            resp = b'Ok...' + data
+            resp = b'Ok...' + buf 
             client_conn.sendall(resp)
         client_conn.close()
