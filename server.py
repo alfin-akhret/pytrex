0e6e4ce456305b713735ad9d236eadda1b801c0a
Alfin Akhret <alfin@qasico.com>
2015-12-17 16:08:34 +0700

    refactoring: create server class

--- server.py
+++ server.py
@@ -6,40 +6,41 @@ from _thread import *
 
 HOST, PORT, RECV_BUFFER = '', 8888, 1024
 
-# create socket object
-# make it listen
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-   
-try:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print('Socket binding failed. Error code:{err_code}. Error message:{err_msg}'.format(err_code=str(msg[0]), err_msg=str(msg[1])))
-    sys.exit()
 
-s.listen(1)
-print('server is listening on {host}:{port}...'.format(host=s.getsockname()[0], port=str(s.getsockname()[1])))
+class server(object):
+    def __init__(self):        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind((HOST, PORT))
+        except socket.error as msg:
+            print('Socket binding failed. Error code:{err_code}. Error message:{err_msg}'.format(err_code=str(msg[0]), err_msg=str(msg[1])))
+            sys.exit()
+        print('Socket created')
 
-# client thread
-# use to maintain connection with connected client
-# using threaded-server technique
-def client_thread(client_conn):
-    while True:
-        data = client_conn.recv(RECV_BUFFER)
-        if not data:
-            break
-        resp = b'Ok...' + data
-        client_conn.sendall(resp)
-    cleint_conn.close()
+    def serv_forever(self, max_conn=1):
+        self.socket.listen(max_conn)
+        while True:
+            client_conn, client_addr = self.socket.accept()
+            print('A client connected from {}'.format(client_addr))
+            start_new_thread(self.client_thread, (client_conn,))
+        self.socket.close()
 
-# listen for new incoming connection
-while True:
-    client_conn, client_addr = s.accept()
-    print('A client connected from {}'.format(client_addr))
-    start_new_thread(client_thread, (client_conn,))
+    def client_thread(self, client_conn):
+        while True:
+            data = client_conn.recv(RECV_BUFFER)
+            if not data:
+                break
+            resp = b'Ok...' + data
+            client_conn.sendall(resp)
+        client_conn.close()
 
-# close listening socket
-s.close()
-   
+
+if __name__ == '__main__':
+    s = server()
+    s.serv_forever()
+
+
+
+  
 
