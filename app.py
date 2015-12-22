e96927464d2e627334913084bb9fd0194366ac1b
Alfin Akhret <alfin@qasico.com>
2015-12-23 02:46:31 +0700

    add low_level/TCPUtils module

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 
 from pytrex import httpserver
 from pytrex import PytrexHttp
+from pytrex.low_level import TCPUtils
 
 if __name__ == '__main__':
     # httpserver.serve_forever()
-    pytrex_server = PytrexHttp.PytrexHttp()
-    pytrex_server.serve()
+    # pytrex_server = PytrexHttp.PytrexHttp()
+    # pytrex_server.serve()
+    
+    tcp_server = TCPUtils.TCPConnection(host='127.0.0.1')
